f8f4694ff1164adbbb0d82c90a1004f36e6c24e5
hnzzchenli <chli924@163.com>
2022-06-15 00:25:52 +0800

    second commit

--- src/main/java/com/atguigu/git/Git_Test.java
+++ src/main/java/com/atguigu/git/Git_Test.java
@@ -6,6 +6,8 @@ package com.atguigu.git;
 public class Git_Test {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello git2");
+        System.out.println("hello git3");
 
     }
 }
