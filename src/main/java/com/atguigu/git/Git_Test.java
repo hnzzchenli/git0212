c1ef99ff671a012d1fab5055935b9cf3961f1d80
hnzzchenli <chli924@163.com>
2022-06-15 00:23:42 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/Git_Test.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * Created by chlistart on 2022/6/15.
+ */
+public class Git_Test {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+
+    }
+}
