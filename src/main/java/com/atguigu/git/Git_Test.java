83c8e02d78c3abf33e05ac0ef62e40a650876c98
hnzzchenli <chli924@163.com>
2022-06-15 08:48:22 +0800

    hf test commit

--- src/main/java/com/atguigu/git/Git_Test.java
+++ src/main/java/com/atguigu/git/Git_Test.java
@@ -10,6 +10,7 @@ public class Git_Test {
         System.out.println("hello git3");
         System.out.println("hello git4");
         System.out.println("hello git5");
+        System.out.println("hot-fix test");
 
     }
 }
